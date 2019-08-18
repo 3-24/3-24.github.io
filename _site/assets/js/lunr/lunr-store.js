968c0d5589d70aa511dc111c0fa632c2250e307e
3-24 <dev.youngseok@gmail.com>
2019-08-19 03:42:23 +0900

    Write test post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,6 @@
-var store = []
+var store = [{
+        "title": "Test for New Engine!",
+        "excerpt":"Hello World! 안녕하세요!   $ \\int _0 ^1 f(x) dx $    ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/test/",
+        "teaser":null}]
