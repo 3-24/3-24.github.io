07d784282685fa3eecd3bb684dd71938879c3051
3-24 <dev.youngseok@gmail.com>
2019-10-31 02:48:23 +0900

    generate

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -129,6 +129,11 @@ var store = [{
         "tags": ["uim"],
         "url": "http://localhost:4000/%EC%9A%B0%EB%B6%84%ED%88%AC-%ED%95%9C%EA%B8%80/",
         "teaser":null},{
+        "title": "리눅스 x86 호출규약",
+        "excerpt":"32비트 리눅스의 호출 규약Calling Convention에 대해서 알아보자. 스택 메모리에서 서브루틴은 호출자의 아래쪽으로 생성된다. ebp 레지스터 위에 함수의 return address가 있고, 그 위에 인자argument가 쌓여있다. 예 int add(int a, int b){ return a+b; } int main(){ int a,b,c; a = 3; b = 7; c = a + b; return 0;...","categories": ["정보보안","pwnable"],
+        "tags": [],
+        "url": "http://localhost:4000/%EC%A0%95%EB%B3%B4%EB%B3%B4%EC%95%88/pwnable/x86callconv/",
+        "teaser":null},{
         "title": "출결 제도에 관한 생각",
         "excerpt":"대학에 다니면서 아침잠이 급격하게 늘어나면서 오전 강의에 늦거나 안 가는 일이 많다. 그런데 늦게 일어나더라도 출석체크가 있는 수업에는 가지 않고 없는 수업에는 가게 된다. 생각의 흐름을 정리해보면 다음과 같다. 출석을 부르는 강의 출결 제도는 학생들의 수업 참여율을 강제하기 위해서 도입한 시스템이다. 그러나 이미 결석이 확실시되면 그 행위를 설명하기 위해 수업을...","categories": ["일상"],
         "tags": [],
